7951653c5e33c5014b2c16f24663ec3f464441cd
Wout Feys <bitterpanda@proton.me>
2025-02-12 15:10:45 +0100

    os.go execute the scan

--- internal/orchestrion/os/os.go
+++ internal/orchestrion/os/os.go
@@ -1,13 +1,12 @@
 package os
 
 import (
-	"errors"
-	"strings"
+	"github.com/AikidoSec/firewall-go/internal/vulnerabilities"
+	"github.com/AikidoSec/firewall-go/internal/vulnerabilities/path_traversal"
 )
 
 func Examine(path string) error {
-	if strings.Contains(path, "blogs") {
-		return errors.New("Attack detected")
-	}
-	return nil
+	return vulnerabilities.Scan(ctx, path_traversal.PathTraversalVulnerability, []string{
+		path /* checkPathStart */, "1",
+	})
 }
