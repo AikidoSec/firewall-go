8af32c4fbad57ef661477473eb069cbae05aad73
Wout Feys <bitterpanda@proton.me>
2025-02-12 13:59:07 +0100

    Gin create context

--- internal/orchestrion/gin-gonic/gin.go
+++ internal/orchestrion/gin-gonic/gin.go
@@ -2,7 +2,9 @@ package gin_gonic
 
 import (
 	"fmt"
+	"github.com/AikidoSec/firewall-go/internal/context"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // GetMiddleware returns middleware that will create contexts of incoming requests. If service is empty then the
@@ -13,5 +15,35 @@ func GetMiddleware() gin.HandlerFunc {
 		fmt.Println(c.Request)
 		fmt.Println(c.Request.URL.Query())
 		c.Next()
+		ginContext := context.Context{
+			URL:            c.Request.URL,
+			Method:         &c.Request.Method,
+			Query:          c.Request.URL.Query(),
+			Headers:        headersToMap(c.Request.Header),
+			RouteParams:    nil,
+			Body:           nil,
+			Cookies:        cookiesToMap(c.Request.Cookies()),
+			AttackDetected: nil,
+			Source:         "gin",
+			Route:          nil,
+			Subdomains:     []string{},
+		}
+		fmt.Println(ginContext)
 	}
+}
+
+func headersToMap(headers http.Header) map[string][]string {
+	headerInfo := make(map[string][]string)
+	for key, values := range headers {
+		headerInfo[key] = values
+	}
+	return headerInfo
+}
+
+func cookiesToMap(cookies []*http.Cookie) map[string]string {
+	cookieInfo := make(map[string]string)
+	for _, cookie := range cookies {
+		cookieInfo[cookie.Name] = cookie.Value
+	}
+	return cookieInfo
 }
