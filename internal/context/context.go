39d160ac5695aa53660752050f0bb5e13b631b69
Wout Feys <bitterpanda@proton.me>
2025-02-12 13:59:01 +0100

    Define Context type

--- internal/context/context.go
+++ internal/context/context.go
@@ -1,1 +1,19 @@
 package context
+
+import "net/url"
+
+type Context struct {
+	URL                *url.URL            `json:"url,omitempty"`
+	Method             *string             `json:"method,omitempty"`
+	Query              map[string][]string `json:"query"`
+	Headers            map[string][]string `json:"headers"`
+	RouteParams        map[string]string   `json:"routeParams,omitempty"`
+	RemoteAddress      *string             `json:"remoteAddress,omitempty"`
+	Body               interface{}         `json:"body"`
+	Cookies            map[string]string   `json:"cookies"`
+	AttackDetected     *bool               `json:"attackDetected,omitempty"`
+	Source             string              `json:"source"`
+	Route              *string             `json:"route,omitempty"`
+	Subdomains         []string            `json:"subdomains,omitempty"`
+	ExecutedMiddleware *bool               `json:"executedMiddleware,omitempty"`
+}
