2c69ece9e3b095bfa5f2c1c3361e3e240898e144
Wout Feys <bitterpanda@proton.me>
2025-02-13 17:57:27 +0100

    Scan SQL

--- internal/sinks/database_sql/main.go
+++ internal/sinks/database_sql/main.go
@@ -1,8 +1,21 @@
 package database_sql
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/AikidoSec/firewall-go/internal/context"
+	"github.com/AikidoSec/firewall-go/internal/vulnerabilities"
+	"github.com/AikidoSec/firewall-go/internal/vulnerabilities/sql_injection"
+)
 
 func Examine(query string) error {
 	fmt.Println("Examining query:", query)
-	return nil
+
+	ctx := context.Get()
+	if ctx == nil {
+		return nil
+	}
+
+	return vulnerabilities.Scan(*ctx, sql_injection.SQLInjectionVulnerability, []string{
+		query /* dialect */, "default",
+	})
 }
