d0352d8406c63dab46841c7c9154533a9147f2bd
Wout Feys <bitterpanda@proton.me>
2025-02-17 16:19:06 +0100

    Create echo-postgres app

---
+++ sample-apps/echo-postgres/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/AikidoSec/firewall-go/zen"
+	"github.com/labstack/echo/v4"
+)
+
+var db *DatabaseHelper
+
+func main() {
+	zen.Init()
+	db = NewDatabaseHelper()
+	// Set up Echo router
+	e := echo.New()
+
+	defineStaticRoutes(e)
+	defineApiRoutes(e, db)
+
+	// Start the server
+	e.Start(":8080")
+}
