d66d4bccf5cf6875262add98770dcee55f13a2c7
Wout Feys <bitterpanda@proton.me>
2025-02-18 16:10:40 +0100

    Fix merge issues

--- instrumentation/orchestrion.tool.go
+++ instrumentation/orchestrion.tool.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/AikidoSec/firewall-go/instrumentation/sinks/database_sql"
 	_ "github.com/AikidoSec/firewall-go/instrumentation/sinks/os"
 	_ "github.com/AikidoSec/firewall-go/instrumentation/sources/gin-gonic"
 )
