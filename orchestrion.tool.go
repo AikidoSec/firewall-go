1022115f2b0f78e5395b66e9a8784114abd931ee
Wout Feys <bitterpanda@proton.me>
2025-02-18 10:56:50 +0100

    Add labstack echo import to orchestrion.tool.go

--- orchestrion.tool.go
+++ orchestrion.tool.go
@@ -4,4 +4,5 @@ import (
 	_ "github.com/AikidoSec/firewall-go/internal/sinks/database_sql"
 	_ "github.com/AikidoSec/firewall-go/internal/sinks/os"
 	_ "github.com/AikidoSec/firewall-go/internal/sources/gin-gonic"
+	_ "github.com/AikidoSec/firewall-go/internal/sources/labstack-echo"
 )
