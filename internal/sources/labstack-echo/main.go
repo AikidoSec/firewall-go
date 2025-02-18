e8575e04b4b3d2b02bf51c69033e65e74bfa1be4
Wout Feys <bitterpanda@proton.me>
2025-02-18 09:57:17 +0100

    Start porting some code for labstack echo

---
+++ internal/sources/labstack-echo/main.go
@@ -0,0 +1,38 @@
+package labstack_echo
+
+import (
+	"encoding/json"
+	"github.com/AikidoSec/firewall-go/internal"
+	"github.com/AikidoSec/firewall-go/internal/context"
+	"github.com/AikidoSec/firewall-go/internal/log"
+	"github.com/AikidoSec/firewall-go/internal/sources/functions"
+	"github.com/labstack/echo"
+)
+
+// GetMiddleware returns middleware that will create contexts of incoming requests. If service is empty then the
+// default service name will be used.
+func GetMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			internal.Init()
+			httpRequest := c.Request()
+			if httpRequest == nil {
+				return next(c) // Do not continue.
+			}
+			echoContext := context.GetContext(httpRequest)
+
+			functions.OnInitRequest(echoContext)
+			err := next(c)               // serve the request to the next middleware
+			functions.OnPostRequest(200) // Run post-request logic (should discover route, api spec,...)
+
+			jsonData, err := json.Marshal(echoContext)
+			if err != nil {
+				log.Errorf("Error marshaling to JSON: %v", err)
+			}
+
+			// Print the JSON output
+			log.Debug(string(jsonData))
+			return err
+		}
+	}
+}
