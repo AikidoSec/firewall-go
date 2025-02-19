f78799e9f1cd549e03a3b83267be714acea9a423
BitterPanda63 <bitterpanda@proton.me>
2025-02-19 18:07:09 +0100

    Use c.Path() for full path

--- instrumentation/sources/labstack-echo/main.go
+++ instrumentation/sources/labstack-echo/main.go
@@ -19,7 +19,7 @@ func GetMiddleware() echo.MiddlewareFunc {
 			if httpRequest == nil {
 				return next(c) // Do not continue.
 			}
-			echoContext := context.GetContext(httpRequest, "echo")
+			echoContext := context.GetContext(httpRequest, c.Path(), "echo")
 
 			functions.OnInitRequest(echoContext)
 			err := next(c)               // serve the request to the next middleware
