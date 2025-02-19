657be0b8542b0ebcacd6235ee124c3936d0e6f28
BitterPanda63 <bitterpanda@proton.me>
2025-02-19 18:34:50 +0100

    Return errors if errors occur

--- sample-apps/echo-postgres/helpers.go
+++ sample-apps/echo-postgres/helpers.go
@@ -8,39 +8,39 @@ import (
 )
 
 // executeShellCommand executes a shell command and returns the output.
-func executeShellCommand(command string) string {
+func executeShellCommand(command string) (string, error) {
 	var output bytes.Buffer
 	cmd := exec.Command("sh", "-c", command)
 	cmd.Stdout = &output
 	cmd.Stderr = &output
 	err := cmd.Run()
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return output.String()
+	return output.String(), nil
 }
 
 // makeHttpRequest makes a simple HTTP GET request and returns the response.
-func makeHttpRequest(url string) string {
+func makeHttpRequest(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return string(body)
+	return string(body), nil
 }
 
 // readFile reads the content of a file and returns it as a string.
-func readFile(filePath string) string {
+func readFile(filePath string) (string, error) {
 	content, err := ioutil.ReadFile("content/blogs/" + filePath)
 	if err != nil {
 
-		panic(err)
+		return "", err
 	}
-	return string(content)
+	return string(content), nil
 }
