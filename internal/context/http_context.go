7499950deb3cab89234c8787f0c16e2d04a2806c
Wout Feys <bitterpanda@proton.me>
2025-02-24 16:04:45 +0100

    Use default false for executedMiddleware in ctx object

--- internal/context/http_context.go
+++ internal/context/http_context.go
@@ -10,18 +10,19 @@ func GetContext(r *http.Request, route string, source string) Context {
 		route = r.URL.Path // Use path from URL as default.
 	}
 	return Context{
-		URL:            fullURL(r),
-		Method:         &r.Method,
-		Query:          r.URL.Query(),
-		Headers:        headersToMap(r.Header),
-		RouteParams:    nil,
-		RemoteAddress:  GetRemoteAddress(r),
-		Body:           nil,
-		Cookies:        cookiesToMap(r.Cookies()),
-		AttackDetected: nil,
-		Source:         source,
-		Route:          route,
-		Subdomains:     []string{},
+		URL:                fullURL(r),
+		Method:             &r.Method,
+		Query:              r.URL.Query(),
+		Headers:            headersToMap(r.Header),
+		RouteParams:        nil,
+		RemoteAddress:      GetRemoteAddress(r),
+		Body:               nil,
+		Cookies:            cookiesToMap(r.Cookies()),
+		AttackDetected:     nil,
+		Source:             source,
+		Route:              route,
+		Subdomains:         []string{},
+		ExecutedMiddleware: false, // We start with no middleware executed.
 	}
 }
 func headersToMap(headers http.Header) map[string][]string {
