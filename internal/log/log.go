67286d973f67facfe269fe122e15daa544ad2c7b
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2025-10-20 11:35:22 +0100

    chore: remove logging to file

--- internal/log/log.go
+++ internal/log/log.go
@@ -21,9 +21,7 @@ var (
 	currentLogLevel = ErrorLevel
 	Logger          = log.New(os.Stdout, "", 0)
 	cliLogging      = true
-	logFilePath     = ""
 )
-var LogFile *os.File
 
 type AikidoFormatter struct{}
 
@@ -48,21 +46,8 @@ func (f *AikidoFormatter) Format(level LogLevel, message string) string {
 	return fmt.Sprintf("[AIKIDO][%s][%s] %s\n", levelStr, time.Now().Format("15:04:05"), message)
 }
 
-func initLogFile() {
-	if LogFile != nil {
-		return
-	}
-	var err error
-	LogFile, err = os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		return
-	}
-	Logger.SetOutput(LogFile)
-}
-
 func logMessage(level LogLevel, args ...interface{}) {
 	if level >= currentLogLevel {
-		initLogFile()
 		formatter := &AikidoFormatter{}
 		message := fmt.Sprint(args...)
 		formattedMessage := formatter.Format(level, message)
@@ -109,7 +94,6 @@ func Warnf(format string, args ...interface{}) {
 
 func Errorf(format string, args ...interface{}) {
 	logMessagef(ErrorLevel, format, args...)
-
 }
 
 func SetLogLevel(level string) error {
@@ -126,10 +110,4 @@ func SetLogLevel(level string) error {
 		return errors.New("invalid log level")
 	}
 	return nil
-}
-
-func Init() {
-	currentTime := time.Now()
-	timeStr := currentTime.Format("20060102150405")
-	logFilePath = fmt.Sprintf("/var/log/aikido/firewall-go-%s-%d.log", timeStr, os.Getpid())
 }
