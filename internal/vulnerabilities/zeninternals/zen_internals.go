331a2d6ae6e1de945c0d5de16839145383c57a20
Tom Aisthorpe <tomaisthorpe@gmail.com>
2025-10-21 15:58:22 +0100

    chore: update zen-internals v0.1.52 (#92)
    * chore: update zen-internals v0.1.51
    
    * chore: update zen-internals v0.1.52

--- internal/vulnerabilities/zeninternals/zen_internals.go
+++ internal/vulnerabilities/zeninternals/zen_internals.go
@@ -4,10 +4,10 @@ package zeninternals
 // #include <dlfcn.h>
 // #include <stdlib.h>
 //
-// typedef int (*detect_sql_injection_func)(const char*, const char*, int);
+// typedef int (*detect_sql_injection_func)(const char*, size_t, const char*, size_t, int);
 //
-// int call_detect_sql_injection(detect_sql_injection_func func, const char* query, const char* input, int sql_dialect) {
-//     return func(query, input, sql_dialect);
+// int call_detect_sql_injection(detect_sql_injection_func func, const char* query, size_t query_len, const char* input, size_t input_len, int sql_dialect) {
+//     return func(query, query_len, input, input_len, sql_dialect);
 // }
 import "C"
 
@@ -69,11 +69,15 @@ func DetectSQLInjection(query string, userInput string, dialect int) int {
 	// Convert strings to C strings
 	cQuery := C.CString(query)
 	cUserInput := C.CString(userInput)
+
 	defer C.free(unsafe.Pointer(cQuery))
 	defer C.free(unsafe.Pointer(cUserInput))
 
+	queryLen := C.size_t(len(query))
+	userInputLen := C.size_t(len(userInput))
+
 	// Call the detect_sql_injection function
-	result := int(C.call_detect_sql_injection(detectSQLInjection, cQuery, cUserInput, C.int(dialect)))
+	result := int(C.call_detect_sql_injection(detectSQLInjection, cQuery, queryLen, cUserInput, userInputLen, C.int(dialect)))
 	log.Debugf("DetectSqlInjection(%s, %s, %d) -> %d", query, userInput, dialect, result)
 	return result
 }
