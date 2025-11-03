42b4d51d604f197187523538297a3a87c0931e82
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2025-11-03 16:02:46 +0000

    Fix nil panic on isObject

--- internal/agent/apidiscovery/get_api_info_test.go
+++ internal/agent/apidiscovery/get_api_info_test.go
@@ -10,6 +10,7 @@ func TestIsObject(t *testing.T) {
 	t.Run("Test isObject", func(t *testing.T) {
 		assert.Equal(t, false, isObject(""))
 		assert.Equal(t, false, isObject([]string{"1"}))
+		assert.Equal(t, false, isObject(nil))
 
 		assert.Equal(t, true, isObject(map[string]any{"1": "2"}))
 		assert.Equal(t, true, isObject(map[string]string{"1": "2"}))
