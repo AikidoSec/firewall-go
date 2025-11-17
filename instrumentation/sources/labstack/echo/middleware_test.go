71846eca5a8444f10139e3325d1e3cb354dc7e0f
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2025-11-17 14:27:55 +0000

    Add tests for request blocking from middlewares

--- instrumentation/sources/labstack/echo/middleware_test.go
+++ instrumentation/sources/labstack/echo/middleware_test.go
@@ -4,10 +4,13 @@ package echo_test
 
 import (
 	"context"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	zenecho "github.com/AikidoSec/firewall-go/instrumentation/sources/labstack/echo"
+	"github.com/AikidoSec/firewall-go/internal/agent/aikido_types"
+	"github.com/AikidoSec/firewall-go/internal/agent/config"
 	"github.com/AikidoSec/firewall-go/internal/request"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
@@ -54,6 +57,90 @@ func TestMiddlewareGLSFallback(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	router.ServeHTTP(w, r)
+}
+
+func TestMiddlewareBlockingRequests(t *testing.T) {
+	block := true
+	config.UpdateServiceConfig(&aikido_types.CloudConfigData{
+		Block: &block,
+		Endpoints: []aikido_types.Endpoint{
+			{
+				Method:             "GET",
+				Route:              "/admin",
+				AllowedIPAddresses: []string{"192.168.0.1"},
+			},
+		},
+	}, &aikido_types.ListsConfigData{
+		BlockedIPAddresses: []aikido_types.BlockedIPsData{
+			{
+				Source:      "test",
+				Description: "localhost",
+				IPs:         []string{"127.0.0.1"},
+			},
+		},
+
+		BlockedUserAgents: "bot.*",
+	})
+
+	router := echo.New()
+	router.Use(zenecho.GetMiddleware())
+
+	router.GET("/route", func(c echo.Context) error {
+		t.Fatal("request should have been blocked")
+		return nil
+	})
+
+	router.GET("/admin", func(c echo.Context) error {
+		return nil
+	})
+
+	t.Run("blocked ip", func(t *testing.T) {
+		r := httptest.NewRequest("GET", "/route", nil)
+		r.RemoteAddr = "127.0.0.1:1234"
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+		assert.Equal(t, http.StatusForbidden, resp.StatusCode)
+	})
+
+	t.Run("blocked user agent", func(t *testing.T) {
+		r := httptest.NewRequest("GET", "/route", nil)
+		r.Header.Set("User-Agent", "bot-test")
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+		assert.Equal(t, http.StatusForbidden, resp.StatusCode)
+	})
+
+	t.Run("block route with unapproved ip", func(t *testing.T) {
+		r := httptest.NewRequest("GET", "/admin", nil)
+		r.RemoteAddr = "192.168.1.1:1234"
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+		assert.Equal(t, http.StatusForbidden, resp.StatusCode)
+	})
+
+	t.Run("allow route with approved ip", func(t *testing.T) {
+		r := httptest.NewRequest("GET", "/admin", nil)
+		r.RemoteAddr = "192.168.0.1:4321"
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		result := w.Result()
+		defer result.Body.Close()
+		assert.Equal(t, http.StatusOK, result.StatusCode)
+	})
 }
 
 func BenchmarkMiddleware(b *testing.B) {
