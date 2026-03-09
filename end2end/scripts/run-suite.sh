211ffc23edba57ea99794431142e2a66c994ac83
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-03-09 12:36:41 +0000

    Prevent apps not being stopped on fail failure

--- end2end/scripts/run-suite.sh
+++ end2end/scripts/run-suite.sh
@@ -118,8 +118,10 @@ echo ""
 echo "Running tests..."
 cd "$END2END_DIR"
 PORT_VALUE=$(cat "/tmp/$APP.port")
+set +e
 APP_NAME=$APP APP_URL="http://localhost:$PORT_VALUE" APP_SQL_DIALECT=$APP_SQL_DIALECT go test -count=1 -v "./tests/$SUITE_NAME/..."
 TEST_EXIT_CODE=$?
+set -e
 
 # Stop the app
 echo ""
