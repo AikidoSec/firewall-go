19c376b7e07ebb1be883a1cd2f32ac37721da452
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-04-14 10:56:38 +0100

    Bump to v1.2.0

--- internal/agent/config/config_test.go
+++ internal/agent/config/config_test.go
@@ -234,7 +234,7 @@ func TestInitWithProvidedEndpoints(t *testing.T) {
 }
 
 func TestVersion(t *testing.T) {
-	require.Equal(t, "1.1.1", Version)
+	require.Equal(t, "1.2.0", Version)
 }
 
 func TestInitReturnsErrorForInvalidConfig(t *testing.T) {
