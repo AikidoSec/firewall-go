b9e4447df723e00c064de1724f522c40ea454c3b
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-05-27 12:58:02 +0100

    Rename op argument to operationName

--- instrumentation/sinks/path/examine.go
+++ instrumentation/sinks/path/examine.go
@@ -12,22 +12,22 @@ import (
 )
 
 // ExamineArg is the hook entry point for functions that take a single string path (e.g. Clean).
-func ExamineArg(op, arg string) error {
-	return Examine(op, []string{arg})
+func ExamineArg(operationName, arg string) error {
+	return Examine(operationName, []string{arg})
 }
 
-func Examine(op string, args []string) error {
+func Examine(operationName string, args []string) error {
 	if zen.IsDisabled() {
 		return nil
 	}
 
-	hooks.OnOperationCall(op, operation.KindFileSystem)
+	hooks.OnOperationCall(operationName, operation.KindFileSystem)
 
 	path := strings.Join(args, "")
 
 	// The error that the vulnerability scan returns is deferred
 	// We delay blocking and reporting until the result is used in os.OpenFile
-	return vulnerabilities.ScanWithOptions(context.Background(), op, pathtraversal.PathTraversalVulnerability, &pathtraversal.ScanArgs{
+	return vulnerabilities.ScanWithOptions(context.Background(), operationName, pathtraversal.PathTraversalVulnerability, &pathtraversal.ScanArgs{
 		FilePath:       path,
 		CheckPathStart: true,
 	}, vulnerabilities.ScanOptions{
