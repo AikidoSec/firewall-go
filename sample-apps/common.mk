bbc22ea41c91a41b1ffcc5f0d18f1d0497a95599
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-06-25 12:26:55 +0200

    Switch all sample apps to use zen-go as go tool

--- sample-apps/common.mk
+++ sample-apps/common.mk
@@ -1,31 +1,27 @@
 PORT ?= 8080
 BINARY := bin/app
 ROOT_DIR := $(shell cd ../.. && pwd)
-ZENGO := $(ROOT_DIR)/tools/bin/zen-go
 
 export AIKIDO_BLOCK ?= true
 export AIKIDO_DEBUG ?= true
 
 .PHONY: build run dev start-database stop-database clean
 
-build: check-zen-go
+build:
 	@mkdir -p bin
-	@$(ZENGO) go build -o $(BINARY) .
+	@go tool zen-go go build -o $(BINARY) .
 
 run: build
 	@PORT=$(PORT) ./$(BINARY)
 
-dev: check-zen-go
-	@PORT=$(PORT) $(ZENGO) go run .
+dev:
+	@PORT=$(PORT) go tool zen-go go run .
 
 start-database:
 	@cd ../databases/ && docker compose up $(DB_SERVICE) -d --wait
 
 stop-database:
 	@cd ../databases/ && docker compose down
-
-check-zen-go:
-	@test -f $(ZENGO) || (echo "❌ zen-go not found at $(ZENGO). Run 'make build-zen-go' from project root" && exit 1)
 
 clean:
 	@rm -rf bin/
