16c2ec95c4c1fde0d42dc87ebea21b96625b81bb
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-07-22 17:21:01 +0100

    Migrate isSafelyEncapsulated tests to testify

--- vulnerabilities/shellinjection/is_safely_encapsulated_test.go
+++ vulnerabilities/shellinjection/is_safely_encapsulated_test.go
@@ -11,81 +11,45 @@ import (
 
 func TestIsSafelyEncapsulated(t *testing.T) {
 	t.Run("safe between single quotes", func(t *testing.T) {
-		if got := isSafelyEncapsulated("echo '$USER'", "$USER"); got != true {
-			t.Errorf("isSafelyEncapsulated('echo '$USER'', '$USER') = %v; want true", got)
-		}
-		if got := isSafelyEncapsulated("echo '`$USER'", "`USER"); got != true {
-			t.Errorf("isSafelyEncapsulated('echo '`$USER'', '`USER') = %v; want true", got)
-		}
+		assert.True(t, isSafelyEncapsulated("echo '$USER'", "$USER"))
+		assert.True(t, isSafelyEncapsulated("echo '`$USER'", "`USER"))
 	})
 
 	t.Run("single quote in single quotes", func(t *testing.T) {
-		if got := isSafelyEncapsulated("echo ''USER'", "'USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo ''USER'', ''USER') = %v; want false", got)
-		}
+		assert.False(t, isSafelyEncapsulated("echo ''USER'", "'USER"))
 	})
 
 	t.Run("dangerous chars between double quotes", func(t *testing.T) {
-		if got := isSafelyEncapsulated(`echo "=USER"`, "=USER"); got != true {
-			t.Errorf("isSafelyEncapsulated('echo \"=USER\"', '=USER') = %v; want true", got)
-		}
-		if got := isSafelyEncapsulated(`echo "$USER"`, "$USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo \"$USER\"', '$USER') = %v; want false", got)
-		}
-		if got := isSafelyEncapsulated(`echo "!USER"`, "!USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo \"!USER\"', '!USER') = %v; want false", got)
-		}
-		if got := isSafelyEncapsulated("echo \"`USER\"", "`USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo \"\\`USER\"', '`USER') = %v; want false", got)
-		}
-		if got := isSafelyEncapsulated(`echo "\\USER"`, "\\USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo \"\\\\USER\"', '\\USER') = %v; want false", got)
-		}
+		assert.True(t, isSafelyEncapsulated(`echo "=USER"`, "=USER"))
+		assert.False(t, isSafelyEncapsulated(`echo "$USER"`, "$USER"))
+		assert.False(t, isSafelyEncapsulated(`echo "!USER"`, "!USER"))
+		assert.False(t, isSafelyEncapsulated("echo \"`USER\"", "`USER"))
+		assert.False(t, isSafelyEncapsulated(`echo "\\USER"`, "\\USER"))
 	})
 
 	t.Run("same user input multiple times", func(t *testing.T) {
-		if got := isSafelyEncapsulated(`echo '$USER' '$USER'`, "$USER"); got != true {
-			t.Errorf("isSafelyEncapsulated('echo '$USER' '$USER'', '$USER') = %v; want true", got)
-		}
-		if got := isSafelyEncapsulated(`echo "$USER" '$USER'`, "$USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo \"$USER\" '$USER'', '$USER') = %v; want false", got)
-		}
-		if got := isSafelyEncapsulated(`echo "$USER" "$USER"`, "$USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo \"$USER\" \"$USER\"', '$USER') = %v; want false", got)
-		}
+		assert.True(t, isSafelyEncapsulated(`echo '$USER' '$USER'`, "$USER"))
+		assert.False(t, isSafelyEncapsulated(`echo "$USER" '$USER'`, "$USER"))
+		assert.False(t, isSafelyEncapsulated(`echo "$USER" "$USER"`, "$USER"))
 	})
 
 	t.Run("the first and last quote doesn't match", func(t *testing.T) {
-		if got := isSafelyEncapsulated(`echo '$USER"`, "$USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo '$USER\"', '$USER') = %v; want false", got)
-		}
-		if got := isSafelyEncapsulated(`echo "$USER'`, "$USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo \"$USER'\", '$USER') = %v; want false", got)
-		}
+		assert.False(t, isSafelyEncapsulated(`echo '$USER"`, "$USER"))
+		assert.False(t, isSafelyEncapsulated(`echo "$USER'`, "$USER"))
 	})
 
 	t.Run("the first or last character is not an escape char", func(t *testing.T) {
-		if got := isSafelyEncapsulated(`echo $USER'`, "$USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo $USER'', '$USER') = %v; want false", got)
-		}
-		if got := isSafelyEncapsulated(`echo $USER"`, "$USER"); got != false {
-			t.Errorf("isSafelyEncapsulated('echo $USER\"', '$USER') = %v; want false", got)
-		}
+		assert.False(t, isSafelyEncapsulated(`echo $USER'`, "$USER"))
+		assert.False(t, isSafelyEncapsulated(`echo $USER"`, "$USER"))
 	})
 
 	t.Run("user input does not occur in the command", func(t *testing.T) {
-		if got := isSafelyEncapsulated(`echo 'USER'`, "$USER"); got != true {
-			t.Errorf("isSafelyEncapsulated('echo 'USER'', '$USER') = %v; want true", got)
-		}
-		if got := isSafelyEncapsulated(`echo "USER"`, "$USER"); got != true {
-			t.Errorf("isSafelyEncapsulated('echo \"USER\"', '$USER') = %v; want true", got)
-		}
+		assert.True(t, isSafelyEncapsulated(`echo 'USER'`, "$USER"))
+		assert.True(t, isSafelyEncapsulated(`echo "USER"`, "$USER"))
 	})
 
 	t.Run("empty user input", func(t *testing.T) {
-		if got := isSafelyEncapsulated("echo hello", ""); got != true {
-			t.Errorf("isSafelyEncapsulated('echo hello', '') = %v; want true", got)
-		}
+		assert.True(t, isSafelyEncapsulated("echo hello", ""))
 	})
 }
 
