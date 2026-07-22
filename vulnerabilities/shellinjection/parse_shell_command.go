02e3b6590e20755eca672a879e9cbbf3dcd9e3d2
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-07-22 13:48:48 +0100

    Detect shell injection when using combined short options

--- vulnerabilities/shellinjection/parse_shell_command.go
+++ vulnerabilities/shellinjection/parse_shell_command.go
@@ -19,13 +19,11 @@ func IsShellCommand(program string) bool {
 }
 
 // ExtractShellCommandString extracts the command string from shell invocation arguments.
-// It looks for the -c flag and returns everything after it as the command string
-// to be interpreted by the shell.
+// It looks for a -c flag (including combined short options like -ec or -cx) and
+// returns everything after it as the command string to be interpreted by the shell.
 func ExtractShellCommandString(args []string) []string {
 	for i := 1; i < len(args); i++ {
-		flag := args[i]
-		if flag == "-c" {
-			// Return everything after the flag
+		if isCommandFlag(args[i]) {
 			if i+1 < len(args) {
 				return args[i+1:]
 			}
@@ -34,4 +32,15 @@ func ExtractShellCommandString(args []string) []string {
 	}
 
 	return nil
+}
+
+// isCommandFlag matches -c anywhere in the flag, since shells parse combined short options as an unordered set.
+func isCommandFlag(flag string) bool {
+	if len(flag) < 2 || flag[0] != '-' || flag[1] == '-' {
+		return false
+	}
+	if strings.Contains(flag, "=") {
+		return false
+	}
+	return strings.ContainsRune(flag[1:], 'c')
 }
